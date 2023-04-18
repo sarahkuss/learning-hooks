325ae51efd5a78b124d0055373ef79459a4120a4
Sarah Kuss <sarah.kuss33@gmail.com>
2023-04-18 10:27:30 -0400

    practiciing useEffect

---
+++ src/components/Message.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react" 
+
+export default function Message() {
+  const [message, setMessage] = useState('Initial Message')
+  useEffect(() => {
+    setMessage('Component did Mount')
+  }, [])
+  return <h2>{message}</h2>
+}
